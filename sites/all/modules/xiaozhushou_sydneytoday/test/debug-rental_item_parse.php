959301697ae12e4abe4029b5b0d5445220de53fa
jeffreycai <jeffreycaizhenyuan@gmail.com>
2013-11-08 16:06:21 +1100

    Continue working on Sydneytoday module cron item

---
+++ sites/all/modules/xiaozhushou_sydneytoday/test/debug-rental_item_parse.php
@@ -0,0 +1,15 @@
+<?php
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+$_SERVER['SCRIPT_NAME'] = 'debug.php';
+
+define('DRUPAL_ROOT','/var/webs/xiaozhushou.com.au');
+require_once(DRUPAL_ROOT . '/includes/bootstrap.inc');
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+module_load_include('inc', 'xiaozhushou_sydneytoday', 'includes/sydneytoday');
+
+
+$item = array(
+    'url' => 'http://www.sydneytoday.com/bencandy.php?fid=12&id=122069'
+);
+sydneytoday_rental_item_parse($item);
