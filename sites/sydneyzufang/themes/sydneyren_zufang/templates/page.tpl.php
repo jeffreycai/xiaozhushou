2b4568082179e507b62704d0eb7373db053dc951
jeffreycai <jeffreycaizhenyuan@gmail.com>
2013-12-10 15:59:25 +1100

    add menu_block module + theme work

--- sites/sydneyzufang/themes/sydneyren_zufang/templates/page.tpl.php
+++ sites/sydneyzufang/themes/sydneyren_zufang/templates/page.tpl.php
@@ -14,7 +14,7 @@
   </nav>
 </div>
 
-<div id="home-page">
+<div id="page">
 
   <header class="header" id="header" role="banner">
 
@@ -42,7 +42,45 @@
 
   <div id="main">
 
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
 
+    <div id="navigation">
+
+        <!--<nav id="main-menu" role="navigation" tabindex="-1">-->
+          <?php print render($page['navigation']); ?>
+        <!--</nav>-->
+
+    </div>
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
 
   </div>
 
