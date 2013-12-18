78329d077b6e6818804bf9e235e35a5b1e8ed3fb
jeffreycai <jeffreycaizhenyuan@gmail.com>
2013-12-18 23:35:34 +1100

    stf

--- sites/sydneyzufang/themes/sydneyren_zufang/template.php
+++ sites/sydneyzufang/themes/sydneyren_zufang/template.php
@@ -130,3 +130,11 @@ function sydneyren_zufang_preprocess_block(&$variables, $hook) {
   //}
 }
 // */
+
+// Add some cool text to the search block form
+function sydneyren_zufang_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    // HTML5 placeholder attribute
+    $form['search_block_form']['#attributes']['placeholder'] = t('请输入地区名，或任意关键字');
+  }
+}
